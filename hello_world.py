0aae79adeb9580705b3406f3662b0361e9302eb4
sahusambit09 <sahusambit09@gma>
2021-05-15 16:35:29 +0530

    13th commit

--- hello_world.py
+++ hello_world.py
@@ -3,6 +3,6 @@ import json
 import django
 # pprint.pprint("hello world")
 print(      "      hello world!!!"                                )
-print("something does not add up fizzzzzzzzzzyyyyyyyy!!!")
+print("something does not add up fizzzzzzzzzzyyyyyyyy!!     !")
 
 
